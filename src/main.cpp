2ab688b7900858c95f1eb9eaf4a0d6be47b9b975
Nicolas Léveillé <nicolas@uucidl.com>
2013-11-26 10:05:31 +0100

    example audio (a drone at around 220hz)
    with beating from left/right channels

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,29 @@
 #endif
 #include <api.h>
 
+extern void render_next_2chn_48khz_audio(uint64_t time_micros,
+                int const sample_count, double left[/*sample_count*/],
+                double right[/*sample_count*/])
+{
+        static double sc_phase = 0.0;
+        static double l_phase = 0.0;
+        static double r_phase = 0.0;
+        double const drone_hz = 220.0;
+
+        for (int i = 0; i < sample_count; i++) {
+                double sincos[2] = {
+                        0.49 * sin(sc_phase),
+                        0.49 * cos(sc_phase),
+                };
+
+                left[i] = 0.5 * sin(l_phase);
+                right[i] = 0.5 * sin(r_phase);
+                l_phase += (1.0f + 0.02f * sincos[0]) * drone_hz * 6.30 / 48000.0;
+                r_phase += (1.0f + 0.02f * sincos[1]) * drone_hz * 6.30 / 48000.0;
+                sc_phase += 6.30 / 48000.0 / 6.0;
+        }
+}
+
 extern void render_next_gl(uint64_t time_micros)
 {
         double const phase = 6.30 * time_micros / 1e6 / 6.0;
